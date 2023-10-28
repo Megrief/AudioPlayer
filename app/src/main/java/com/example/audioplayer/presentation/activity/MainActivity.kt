c00c0c6ae5b55e20432a52bbe2e1a91289f89691
Megrief <eu.pim@mail.ru>
2023-10-28 11:12:46 +0300

    Notification c рабочими кнопками.

--- app/src/main/java/com/example/audioplayer/presentation/activity/MainActivity.kt
+++ app/src/main/java/com/example/audioplayer/presentation/activity/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.example.audioplayer.presentation.activity
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import com.example.audioplayer.databinding.ActivityMainBinding
 import com.example.audioplayer.presentation.activity.adapter.TrackAdapter
+import com.example.audioplayer.presentation.service.PlayerConstants
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
@@ -18,12 +21,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-
+        setButtonsEnabled(false)
         viewModel.registerReceiver(applicationContext)
         viewModel.configurePlayer()
-
+        configurePlayerView()
         binding.contentList.adapter = TrackAdapter {
             lifecycleScope.launch(Dispatchers.IO) {
+                withContext(Dispatchers.Main) {
+                    setButtonsEnabled(true)
+                }
                 viewModel.play(it.id)
                 viewModel.startService(applicationContext)
             }
@@ -31,17 +37,36 @@ class MainActivity : AppCompatActivity() {
         viewModel.listContent.observe(this@MainActivity) { trackList ->
             (binding.contentList.adapter as TrackAdapter).setContent(trackList)
         }
-
     }
 
-    override fun onStart() {
-        super.onStart()
-//        viewModel.bind(this)
+    private fun configurePlayerView() {
+        with(binding) {
+            playButton.setOnClickListener {
+                val intent = Intent(PlayerConstants.ACTION_PLAY)
+                sendBroadcast(intent)
+            }
+            pauseButton.setOnClickListener {
+                val intent = Intent(PlayerConstants.ACTION_PAUSE)
+                sendBroadcast(intent)
+            }
+            previousButton.setOnClickListener {
+                val intent = Intent(PlayerConstants.ACTION_PREVIOUS)
+                sendBroadcast(intent)
+            }
+            nextButton.setOnClickListener {
+                val intent = Intent(PlayerConstants.ACTION_NEXT)
+                sendBroadcast(intent)
+            }
+        }
     }
 
-    override fun onStop() {
-        super.onStop()
-//        viewModel.unbind(this)
+    private fun setButtonsEnabled(isEnabled: Boolean) {
+        with(binding) {
+            playButton.isEnabled = isEnabled
+            pauseButton.isEnabled = isEnabled
+            nextButton.isEnabled = isEnabled
+            previousButton.isEnabled = isEnabled
+        }
     }
 
 }
--- app/src/main/java/com/example/audioplayer/presentation/activity/MainVM.kt
+++ app/src/main/java/com/example/audioplayer/presentation/activity/MainVM.kt
@@ -27,8 +27,6 @@ class MainVM(
     private val bReceiver: AudioPlayerReceiver
 ) : ViewModel() {
 
-//    private var mBound = false
-
     private val _listContent: MutableLiveData<List<Track>> = MutableLiveData(emptyList())
     val listContent: LiveData<List<Track>>
         get() = _listContent
@@ -88,29 +86,4 @@ class MainVM(
         }
 
     }
-
-//    private val serviceConnection = object : ServiceConnection {
-//        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
-//            val binder = service as AudioPlayerService.AudioBinder
-//            binder.getService()
-//            mBound = true
-//        }
-//
-//        override fun onServiceDisconnected(name: ComponentName?) {
-//            mBound = false
-//        }
-//    }
-
-
-//
-//    fun bind(context: Context) {
-//        Intent(context, AudioPlayerService::class.java).also { intent ->
-//            context.bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE)
-//        }
-//    }
-//
-//    fun unbind(context: Context) {
-//        context.unbindService(serviceConnection)
-//        mBound = false
-//    }
 }
